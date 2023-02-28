d068e858063b52712e95962ca1b3dcbc78c760fd
kadir <idia_2008@hotmail.com>
2023-02-28 23:52:15 +0300

    add new file

--- easy.js
+++ easy.js
@@ -18,7 +18,9 @@ import { findIntersection } from "./easy/5_findintersection.js";
 
 //console.log(findIntersection(["1, 3, 4, 7, 13", "1, 2, 4,1 3, 15"]));
 
-import { questionMark } from "./6_questionMarks.js";
+import { questionMark } from "./easy/6_questionMarks.js";
 
-console.log(questionMark("acc?7??sss?3rr1??????5"));
+//console.log(questionMark("acc?7??sss?3rr1??????5"));
+
+import { letterChange } from ".easy/7_letterChange.js";
 
